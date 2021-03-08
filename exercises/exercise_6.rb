4be1704b21f5ceb95d58139b3e2b84061b876a4a
Paxon Cheung <paxon.cheung@gmail.com>
2021-03-08 05:02:35 +0000

    wrap up validations

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Paxon", last_name: "Cheung", hourly_rate: 40)
+@store1.employees.create(first_name: "Andy", last_name: "Lindsay", hourly_rate: 80)
+
+@store2.employees.create(first_name: "Sabrina", last_name: "Yau", hourly_rate: 60)
+@store2.employees.create(first_name: "Travis", last_name: "Borsa", hourly_rate: 50)
+@store2.employees.create(first_name: "Bradley", last_name: "Fung", hourly_rate: 40)
